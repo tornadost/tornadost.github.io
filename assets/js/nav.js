9903e2cc56ce38742a0416fb4fc0189f69f988a7
tornadost <120360241+tornadost@users.noreply.github.com>
2025-05-07 22:20:22 -0400

    add goods

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,10 +1,11 @@
 document.addEventListener('DOMContentLoaded', async () => {
     const placeholder = document.getElementById('nav-placeholder');
     try {
-      const resp = await fetch('/assets/includes/nav.html');
-      if (!resp.ok) throw new Error(resp.statusText);
+      const resp = await fetch('/assets/html/nav.html');
+      if (!resp.ok) throw new Error(`HTTP ${resp.status}`);
       placeholder.innerHTML = await resp.text();
     } catch (err) {
       console.error('Could not load nav:', err);
     }
   });
+  
