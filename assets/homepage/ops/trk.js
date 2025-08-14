16901adbe521c5f64846aaf592d53139ef0825af
tornadost <120360241+tornadost@users.noreply.github.com>
2025-08-14 17:42:23 -0400

    Update trk.js

--- assets/homepage/ops/trk.js
+++ assets/homepage/ops/trk.js
@@ -1,5 +1,6 @@
 export const trackerEnc = [
   "aHR0cHM6Ly9maWxlbGlzdC5pby8=",
+  "aHR0cHM6Ly9waG9lbml4cHJvamVjdC5hcHAvindexLnBocA==",
   "aHR0cHM6Ly9mMWNhcnJlcmFzLnh5ei8=",
   "aHR0cHM6Ly9laWdhLm1vaS91c2Vycy9SZWFudUtlYXZlczg5",
   "aHR0cHM6Ly9oZC1zcGFjZS5vcmcvaW5kZXgucGhwP3BhZ2U9dXNlcmNwJnVpZD0yMzcwNjQ=",
@@ -42,5 +43,6 @@ export const trackerEnc = [
   "aHR0cHM6Ly9jYXB5YmFyYWJyLmNvbS91c2Vycy9BR0NhcnJhcmFGaW5hbEJvc3M=",
   "aHR0cHM6Ly93d3cucmVkZGl0LmNvbS9yL09wZW5TaWdudXBzL25ldy8=",
 ];
+
 
 
