f0b2376bcae019f741a314f47861e7483733e1c9
tornadost <120360241+tornadost@users.noreply.github.com>
2026-04-22 17:21:20 -0400

    Create cht.js

---
+++ assets/homepage/ops/cht.js
@@ -0,0 +1,18 @@
+export const trkEnc = [
+  "Y2FweWJhcmFici5jb20v",
+  "ZmVhcm5vcGVlci5jb20v",
+  "dGhlbGR1LnRvLw==",
+  "YWl0aGVyLmNjLw==",
+  "aG9taWVoZWxwZGVzay5uZXQv",
+  "Ymx1dG9waWEuY2Mv",
+  "b25seWVuY29kZXMuY2Mv",
+  "dXBsb2FkLmN4Lw==",
+  "bHN0LmdnLw==",
+  "eXUtc2NlbmUubmV0Lw==",
+  "cmVlbGZsaXguY2Mv",
+  "cG9ydHVnYXMub3JnLw==",
+  "b2xkdG9vbnMud29ybGQv",
+  "d3d3Lm15YW5vbmFtb3VzZS5uZXQvaW5kZXgucGhw",
+  "ZWlnYS5tb2kv",
+  "ZjFjYXJyZXJhcy54eXov"
+];
