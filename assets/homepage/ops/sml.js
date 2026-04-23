d6c1c7a39109440e7201f2cd5233932e9140922f
tornadost <120360241+tornadost@users.noreply.github.com>
2026-04-22 23:14:23 -0400

    Update sml.js

--- assets/homepage/ops/sml.js
+++ assets/homepage/ops/sml.js
@@ -1,16 +1,14 @@
 export const trkEnc = [
-  "ZGFya3BlZXJzLm9yZy8=",
-  "c3NsLmJvb3R5dGFwZS5jb20vdG9ycmVudHMucGhw",
-  "aHVid2FyZXoudHYvZm9ydW0vY29udGVudC5waHA=",
-  "bWlsa2llLmNjL2Jyb3dzZQ==",
+  "cnV0cmFja2VyLm9yZy9mb3J1bS9wcm9maWxlLnBocD9tb2RlPXZpZXdwcm9maWxlJnU9NTI1OTk4MTI=",
+  "bG9zc2xlc3NjbHViLmNvbS91c2VyZGV0YWlscy5waHA/aWQ9Njg1Nzg=",  
   "aGRmYW5zLm9yZy9pbmRleC5waHA=",
   "cHQuYnRzY2hvb2wuY2x1Yi9pbmRleC5waHA=",
   "Y2FycHQubmV0L2luZGV4LnBocA==",
+  "c3NsLmJvb3R5dGFwZS5jb20vdXNlcmRldGFpbHMucGhwP2lkPTkyNjc5NQ==",
+  "bWlsa2llLmNjL2Jyb3dzZQ==",
   "aXRhdG9ycmVudHMueHl6Lw==",
-  "d3d3LmRhdGFzY2VuZS54eXov",
-  "dXBzY2FsZXZhdWx0LmNvbS91c2Vycy9SZWFudUtlYXZlczg5",
-  "cnV0cmFja2VyLm9yZy9mb3J1bS9zZWFyY2gucGhw",
-  "c2FtYXJpdGFuby5jYy9wYWdlcy8x",
-  "bG9zc2xlc3NjbHViLmNvbS91c2VyZGV0YWlscy5waHA/aWQ9Njg1Nzg=",
-  "d3d3LnJlZGRpdC5jb20vci9PcGVuU2lnbnVwcy8="
+  "ZGF0YXNjZW5lLnh5ei8=",
+  "dXBzY2FsZXZhdWx0LmNvbS91c2Vycy9SZWFudUtlYXZlczg5",  
+  "c2FtYXJpdGFuby5jYy8=",
+  "ZGFya3BlZXJzLm9yZy8="
 ];
